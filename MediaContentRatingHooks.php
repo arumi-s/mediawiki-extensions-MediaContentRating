7a598128929268b2e8df31515aa554b95ddb46ae
Arumi Sakura <arumi@thwiki.cc>
2024-02-29 00:23:33 +0800

    Update MediaContentRatingHooks.php

--- MediaContentRatingHooks.php
+++ MediaContentRatingHooks.php
@@ -6,8 +6,7 @@
  * @author Arumi
  */
 
-class MediaContentRatingHooks
-{
+class MediaContentRatingHooks {
 	/**
 	 * @var map[string]string Content rating alias
 	 */
@@ -23,26 +22,25 @@ class MediaContentRatingHooks
 	 * @param User $user
 	 * @param array &$preferences
 	 */
-	public static function onGetPreferences(User $user, array &$preferences)
-	{
-		if ($user->isAnon() || $user->isBlocked()) {
+	public static function onGetPreferences( User $user, array &$preferences ) {
+		if ( $user->isAnon() || $user->isBlocked() ) {
 			return true;
 		}
 
 		$option = [
-			wfMessage('content-rating-show')->text() => '1',
-			wfMessage('content-rating-hide')->text() => '0',
+			wfMessage( 'content-rating-show' )->text() => '1',
+			wfMessage( 'content-rating-hide' )->text() => '0',
 		];
 
-		foreach (self::$msgs as $rate => $msg) {
-			$name = 'cr-allow-' . strtolower($rate);
+		foreach ( self::$msgs as $rate => $msg ) {
+			$name = 'cr-allow-' . strtolower( $rate );
 			$preferences[$name] = [
 				'type' => 'radio',
-				'label-message' => 'content-rating-' . strtolower($rate),
+				'label-message' => 'content-rating-' . strtolower( $rate ),
 				'section' => 'rendering/content-rating',
 				'options' => $option,
 				'flatlist' => true,
-				'default' => $user->getOption($name, false) ? '1' : '0',
+				'default' => $user->getOption( $name, false ) ? '1' : '0',
 			];
 		}
 		return true;
@@ -53,11 +51,8 @@ class MediaContentRatingHooks
 	 *
 	 * @param Parser $parser the Parser object
 	 */
-	public static function onParserFirstCallInit(Parser $parser)
-	{
-		$parser->setFunctionHook('rating', [__CLASS__, 'rating']);
-
-		$parser->setHook('rating', [__CLASS__, 'ratingTag']);
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook( 'rating', [ __CLASS__, 'rating' ] );
 	}
 
 	/**
@@ -67,36 +62,17 @@ class MediaContentRatingHooks
 	 * @param string $text rating string
 	 * @return string normalized rating string
 	 */
-	public static function rating(Parser $parser, $text = '')
-	{
+	public static function rating( Parser $parser, $text = '' ) {
 		$output = $parser->getOutput();
-		$rate = self::mapContentRating($text);
+		$rate = self::mapContentRating( $text );
 
-		if ($rate !== '') {
-			$output->setProperty('content-rating', $rate);
+		if ( $rate !== '' ) {
+			$output->setProperty( 'content-rating', $rate );
 		} else {
-			$output->unsetProperty('content-rating');
+			$output->unsetProperty( 'content-rating' );
 		}
 
 		return $rate;
-	}
-
-	/**
-	 * Add a start or end marker to content
-	 *
-	 * @param string|null $in The text inside the tag
-	 * @param string[] $param
-	 * @param Parser $parser
-	 * @param PPFrame $frame
-	 * @return string
-	 */
-	public static function ratingTag($in, array $param, Parser $parser, PPFrame $frame)
-	{
-		$startRating = isset($param['start']) ? strtolower(self::mapContentRating($param['start'])) : '';
-		$endRating = isset($param['end']) ? strtolower(self::mapContentRating($param['end'])) : '';
-
-		return ($endRating !== '' ? '<rating-end-' . $endRating . '></rating-end-' . $endRating . '>' : '') .
-			($startRating !== '' ? '<rating-start-' . $startRating . '></rating-start-' . $startRating . '>' : '');
 	}
 
 	/**
@@ -106,28 +82,19 @@ class MediaContentRatingHooks
 	 * @param string &$text
 	 * @return bool Always <code>true</code>
 	 */
-	public static function onOutputPageBeforeHTML(OutputPage $output, string &$text)
-	{
+	public static function onOutputPageBeforeHTML( OutputPage $output, string &$text ) {
 		$user = $output->getUser();
 
-		if (!($user instanceof User) || $user->isAnon()) {
-			$text = preg_replace('/<rating-start-([^>]*)><\\/rating-start-\\1>.*?<rating-end-\\1><\\/rating-end-\\1>/s', '', $text);
-			$text = preg_replace('/<!--cr-.*?-cr-->/s', '', $text);
+		if ( !( $user instanceof User ) || $user->isAnon() ) {
+			$text = preg_replace( '/<!--cr-.*?-cr-->/', '', $text );
 			return true;
 		}
-		foreach (self::$msgs as $rate => $msg) {
-			$rate = strtolower($rate);
-			$name = 'cr-allow-' . $rate;
-			if ($user->getOption($name, false)) {
-				$text = str_replace([
-					'<rating-start-' . $rate . '></rating-start-' . $rate . '>',
-					'<rating-end-' . $rate . '></rating-end-' . $rate . '>',
-					'<!--cr-' . $rate . '-',
-					'-' . $rate . '-cr' . '-->',
-				], '', $text);
+		foreach ( self::$msgs as $rate => $msg ) {
+			$name = 'cr-allow-' . strtolower( $rate );
+			if ( $user->getOption( $name, false ) ) {
+				$text = str_replace( [ '<!--cr-' . strtolower( $rate ) . '-', '-' . strtolower( $rate ) . '-cr' . '-->' ], '', $text );
 			} else {
-				$text = preg_replace('/<rating-start-' . $rate . '><\\/rating-start-' . $rate . '>.*?<rating-end-' . $rate . '><\\/rating-end-' . $rate . '>/s', '', $text);
-				$text = preg_replace('/<!--cr-' . $rate . '.*?-' . $rate . '-cr-->/s', '', $text);
+				$text = preg_replace( '/<!--cr-' . strtolower( $rate ) . '.*?-' . strtolower( $rate ) . '-cr-->/', '', $text );
 			}
 		}
 
@@ -143,12 +110,11 @@ class MediaContentRatingHooks
 	 * @param array &$frameParams
 	 * @return bool Always <code>true</code>
 	 */
-	public static function onImageBeforeProduceHTML(DummyLinker &$linker, Title &$title, &$file, array &$frameParams)
-	{
-		$rate = self::getContentRating($title);
-		if (!empty($rate)) {
-			$frameParams['prefix'] = '<!--cr-' . strtolower($rate) . '-';
-			$frameParams['postfix'] = '-' . strtolower($rate) . '-cr-->';
+	public static function onImageBeforeProduceHTML( DummyLinker &$linker, Title &$title, &$file, array &$frameParams ) {
+		$rate = self::getContentRating( $title );
+		if ( !empty( $rate ) ) {
+			$frameParams['prefix'] = '<!--cr-' . strtolower( $rate ) . '-';
+			$frameParams['postfix'] = '-' . strtolower( $rate ) . '-cr-->';
 		}
 
 		return true;
@@ -161,19 +127,18 @@ class MediaContentRatingHooks
 	 * @param OutputPage $output
 	 * @return bool Always <code>true</code>
 	 */
-	public static function onImageOpenShowImageInlineBefore(ImagePage $imagePage, OutputPage $output)
-	{
+	public static function onImageOpenShowImageInlineBefore( ImagePage $imagePage, OutputPage $output ) {
 		$mediaContentRatingWarningImage = $imagePage->getContext()
 			->getConfig()
-			->get('MediaContentRatingWarningImage');
+			->get( 'MediaContentRatingWarningImage' );
 
 		$file = $imagePage->getDisplayedFile();
-		$rate = self::getContentRating($file);
+		$rate = self::getContentRating( $file );
 
-		if (self::isUserAllowed($imagePage->getContext()->getUser(), $rate)) {
+		if ( self::isUserAllowed( $imagePage->getContext()->getUser(), $rate ) ) {
 			return true;
 		}
-		$imagePage->setFile(wfFindFile($mediaContentRatingWarningImage));
+		$imagePage->setFile( wfFindFile( $mediaContentRatingWarningImage ) );
 
 		return true;
 	}
@@ -188,43 +153,76 @@ class MediaContentRatingHooks
 	 * @param array &$handlerOpts
 	 * @return bool Always <code>true</code>
 	 */
-	public static function onBeforeAddToGallery(&$title, &$html, &$alt, &$link, &$handlerOpts)
-	{
+	public static function onBeforeAddToGallery( &$title, &$html, &$alt, &$link, &$handlerOpts ) {
 		$user = RequestContext::getMain()->getUser();
-		$rate = self::getContentRating($title);
-		return self::isUserAllowed($user, $rate);
+		$rate = self::getContentRating( $title );
+		return self::isUserAllowed( $user, $rate );
+	}
+
+	/**
+	 * Prevent restricted image from chosen as page images
+	 *
+	 * @param array $image Associative array describing an image
+	 * @param int $position Image order on page
+	 * @param float &$score Score for image
+	 * @return bool Always <code>true</code>
+	 */
+	public static function onPageImagesGetScore( array $image, $position, &$score ) {
+		if ( !empty( self::getContentRating( wfFindFile( $image['filename'] ) ) ) ) {
+			$score = -1000;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Remove restricted image from image list
+	 *
+	 * @param ImageListPager $imageListPager
+	 * @param File $file
+	 * @param string &$html
+	 * @return bool
+	 */
+	public static function onBeforeAddToImageList( ImageListPager $imageListPager, File $file, string &$html ) {
+		$user = $imageListPager->getUser();
+		$rate = self::getContentRating( $file );
+		if ( !self::isUserAllowed( $user, $rate ) ) {
+			$html = '';
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
 	 * @param File|Title|string $text
 	 * @return string
 	 */
-	public static function getContentRating($text)
-	{
+	public static function getContentRating( $text ) {
 		static $cache = [];
 
-		if ($text instanceof File) {
+		if ( $text instanceof File ) {
 			$id = $text->getTitle()->getArticleID();
-		} elseif ($text instanceof Title) {
+		} elseif ( $text instanceof Title ) {
 			$id = $text->getArticleID();
 		} else {
-			$title = Title::newFromText($text);
+			$title = Title::newFromText( $text );
 			$id = $title instanceof Title ? $title->getArticleID() : 0;
 		}
-		if ($id == 0) {
+		if ( $id == 0 ) {
 			return false;
 		}
 
-		if (isset($cache[$id])) {
+		if ( isset( $cache[$id] ) ) {
 			return $cache[$id];
 		}
 
-		$dbr = wfGetDB(DB_REPLICA);
+		$dbr = wfGetDB( DB_REPLICA );
 
 		$cache[$id] = $dbr->selectField(
 			'page_props',
 			'pp_value',
-			['pp_propname' => 'content-rating', 'pp_page' => $id]
+			[ 'pp_propname' => 'content-rating', 'pp_page' => $id ]
 		);
 
 		return $cache[$id];
@@ -235,44 +233,42 @@ class MediaContentRatingHooks
 	 * @param string|bool $rate
 	 * @return bool
 	 */
-	private static function isUserAllowed(User $user, $rate = false)
-	{
-		if (empty($rate)) {
+	private static function isUserAllowed( User $user, $rate = false ) {
+		if ( empty( $rate ) ) {
 			return true;
 		}
-		if (!($user instanceof User) || $user->isAnon()) {
+		if ( !( $user instanceof User ) || $user->isAnon() ) {
 			return false;
 		}
 
-		return (bool) $user->getOption('cr-allow-' . strtolower($rate), false);
+		return (bool)$user->getOption( 'cr-allow-' . strtolower( $rate ), false );
 	}
 
 	/**
 	 * @param string $text
 	 * @return string
 	 */
-	private static function mapContentRating($text = '')
-	{
+	private static function mapContentRating( $text = '' ) {
 		static $map = null;
-		$text = trim($text);
-		if ($text === '') {
+		$text = trim( $text );
+		if ( $text === '' ) {
 			return '';
 		}
 
-		if ($map == null) {
+		if ( $map == null ) {
 			$map = [];
-			foreach (self::$msgs as $rate => $msg) {
-				foreach (explode(',', $msg) as $name) {
-					$name = trim($name);
-					if ($name !== '') {
+			foreach ( self::$msgs as $rate => $msg ) {
+				foreach ( explode( ',', $msg ) as $name ) {
+					$name = trim( $name );
+					if ( $name !== '' ) {
 						$map[$name] = $rate;
 					}
 				}
 			}
 		}
-		$text = strtoupper($text);
+		$text = strtoupper( $text );
 
-		if (isset($map[$text])) {
+		if ( isset( $map[$text] ) ) {
 			return $map[$text];
 		} else {
 			return '';
